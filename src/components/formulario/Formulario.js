1c01feee3e4e470febb3be09eb572e94039f624c
guerraajuan <guerraelec@gmail.com>
2021-09-14 19:15:38 -0300

    ajustes

--- src/components/formulario/Formulario.js
+++ src/components/formulario/Formulario.js
@@ -143,7 +143,7 @@ const Formulario = () =>{
 
 
             setMsjBoton("Sus datos fueron recibidos, recibirá un email de confirmacion");
-            etMsjNombre("");
+            setMsjNombre("");
             
         }
         else{
